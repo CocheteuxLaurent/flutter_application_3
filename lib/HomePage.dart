02bf5ce8873678d2321661052ff46cf1183fd47d
CocheteuxLaurent <laurent.cocheteux@hotmail.fr>
2022-10-04 18:44:33 +0200

    update

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/rendering/box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: /*SafeArea(
         child: Column(
           children: [
             Row(
@@ -66,6 +67,60 @@ class _HomePageState extends State<HomePage> {
                     ),
                   );
                 }),
+          ],
+        ),
+      ),
+      */
+          SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Hello !',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Container(
+                width: 350,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.black,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(35),
+                        borderSide: BorderSide.none),
+                    labelText: 'Enter name of movie',
+                    labelStyle: TextStyle(fontSize: 12, color: Colors.white),
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 100,
+                      childAspectRatio: 3 / 2,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20),
+                  itemCount: 2,
+                  itemBuilder: (BuildContext context, index) {
+                    return Card(child: Text('TEST'));
+                  }),
+            )
           ],
         ),
       ),
